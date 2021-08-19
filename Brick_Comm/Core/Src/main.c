1c982dbef7cf3fa91c28855f26e097bfa8ff5634
Grant Geyer <GrantG012@yahoo.com>
2021-08-18 19:00:59 -0500

    Rename Bricks project, use common folder, and remove driver code

--- Brick_Comm/Core/Src/main.c
+++ Brick_Comm/Core/Src/main.c
@@ -24,7 +24,7 @@
 /* USER CODE BEGIN Includes */
 
 #include "canFilterBankConfig.h"
-#include "queue_api.h"
+#include "queue.h"
 
 /* USER CODE END Includes */
 
@@ -33,18 +33,18 @@
 
 typedef struct
 {
-	CAN_TxHeaderTypeDef canTxHeader;
-	uint8_t data[8];
+    CAN_TxHeaderTypeDef canTxHeader;
+    uint8_t data[8];
 } CanTxData;
 
 typedef struct
 {
-	//  Parameters to store
-	uint8_t num_data;
-	uint16_t command;
-	uint8_t data[8];
-	uint16_t dev_address;
-	uint8_t read_write;
+    //  Parameters to store
+    uint8_t num_data;
+    uint16_t command;
+    uint8_t data[8];
+    uint16_t dev_address;
+    uint8_t read_write;
 } I2CTxData;
 
 /* USER CODE END PTD */
@@ -59,7 +59,7 @@ typedef struct
 #define POWER_BRICK_OPERATION_CAN_FIFO_NUMBER 1
 #define POWER_BRICK_OPERATION_CAN_FILTER_BANK_NUMBER 1
 
-#define POWER_BRICK_REPLY_CAN_ID 0x215
+#define POWER_BRICK_REPLY_CAN_ID 0x305
 
 #define NUM_CAN_TX_QUEUE_MESSAGES 5
 #define NUM_I2C_TX_QUEUE_MESSAGES 10
@@ -82,13 +82,13 @@ I2C_HandleTypeDef hi2c1;
 
 /* USER CODE BEGIN PV */
 
-int canTxQueueHandle;
+queue_handle_t canTxQueueHandle;
 CanTxData canTxData[NUM_CAN_TX_QUEUE_MESSAGES];
 CanTxData canTxPrivateMessageToSend;
 
-int i2cTxQueueHandle;
+queue_handle_t i2cTxQueueHandle;
 I2CTxData i2cTxData[NUM_I2C_TX_QUEUE_MESSAGES];
-I2CTxData* i2cTxPrivateMessageToSend;
+I2CTxData *i2cTxPrivateMessageToSend;
 
 int i2cTransactionReadyToStart = 1;
 I2CTxData i2c_transfer_out_node;
@@ -156,83 +156,39 @@ int main(void)
 
   HAL_CAN_Start(&hcan);
 
-  /*
-  //  Create CAN Message
-  canTxPrivateMessageToSend.canTxHeader.StdId = 0x215;
-  canTxPrivateMessageToSend.canTxHeader.DLC = 4;
-  canTxPrivateMessageToSend.canTxHeader.IDE = CAN_ID_STD;
-  canTxPrivateMessageToSend.canTxHeader.RTR = CAN_RTR_DATA;
-  canTxPrivateMessageToSend.data[0] = 0x01;
-  canTxPrivateMessageToSend.data[1] = 0x04;
-  canTxPrivateMessageToSend.data[2] = 0x09;
-  canTxPrivateMessageToSend.data[3] = 0x10;
-
-  SendCANMessage(&canTxPrivateMessageToSend);
-  */
-
-  //Use variable hi2c1 if a I2C_HandleTypeDef is needed
-//  uint8_t temp_request_code = 0x8D; // The request code for asking for temperature
-//  uint8_t temp_receive_1; // The char that will hold the temperature return data of slave 1
-//  uint8_t temp_receive_2; // The char that will hold the temperature return data of slave 2
-//  uint8_t receiving_array[2] = {};
-//  uint8_t command_code = 0x01;
-//  uint8_t zeros = 0x0;
-//  uint8_t i2c_read_array[1] = {};
-
-  //HAL_I2C_Mem_Read(&hi2c1, slave_address_1 << 1, temp_request_code, 1,
-		  //receiving_array, sizeof(uint16_t), 1000000);
-  //HAL_StatusTypeDef HAL_I2C_Mem_Read(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress,
-  //uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout)
-    // Blocking Statements
-  // if writing() HAL_I2C_Mem_Write(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress,
-  //uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout)
-
-  /*
-  hi2c1.Instance->CR2 |= I2C_CR2_PECBYTE;
-  HAL_I2C_Mem_Write(&hi2c1, slave_address_1 << 1, command_code, 1, &zeros, 2, 1000);
-  HAL_I2C_Mem_Read(&hi2c1, slave_address_1 << 1, command_code, 1,
-  		  i2c_read_array, sizeof(uint8_t), 1000);
-  */
-
-  	  //Interrupt Statements
-  //HAL_I2C_RegisterCallback(&hi2c1, HAL_I2C_MASTER_TX_COMPLETE_CB_ID, );
-  //HAL_I2C_Master_Transmit_IT(&hi2c1, slave_address_1 << 1, &temp_request_code, sizeof(uint8_t));
-  // Transmits the hex code to the first slave to request temperature
-  //HAL_I2C_Master_Receive_IT(&hi2c1, slave_address_1 << 1, &temp_receive_1, sizeof(uint8_t));
-  // Receives the temperature response of the first slave
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 
-  while (1)
-  {
-	  if (!isQueueEmpty(i2cTxQueueHandle) && i2cTransactionReadyToStart)
-	  	  {
-		  	  i2cTransactionReadyToStart = 0;
+    while (1)
+    {
+        if (!isQueueEmpty(i2cTxQueueHandle) && i2cTransactionReadyToStart)
+        {
+            i2cTransactionReadyToStart = 0;
 
-		  	  HAL_NVIC_DisableIRQ(CEC_CAN_IRQn);
-		  	  RemoveFromQueue(i2cTxQueueHandle, &i2c_transfer_out_node);
-		  	  HAL_NVIC_EnableIRQ(CEC_CAN_IRQn);
+            HAL_NVIC_DisableIRQ(CEC_CAN_IRQn);
+            RemoveFromQueue(i2cTxQueueHandle, &i2c_transfer_out_node);
+            HAL_NVIC_EnableIRQ(CEC_CAN_IRQn);
 
-		  	  if (i2c_transfer_out_node.read_write) //It's a read
-		  	  {
-		  		HAL_I2C_Mem_Read_IT(&hi2c1, i2c_transfer_out_node.dev_address << 1, i2c_transfer_out_node.command,
-		  				1, i2c_transfer_out_node.data, i2c_transfer_out_node.num_data);
-		  	  }
-		  	  else //It's a write
-		  	  {
-		  		hi2c1.Instance->CR2 |= I2C_CR2_PECBYTE;
-		  		HAL_I2C_Mem_Write_IT(&hi2c1, i2c_transfer_out_node.dev_address << 1, i2c_transfer_out_node.command,
-		  				1, i2c_transfer_out_node.data, i2c_transfer_out_node.num_data + 1);
-		  	  }
-	  	  }
+            if (i2c_transfer_out_node.read_write) // It's a read
+            {
+                HAL_I2C_Mem_Read_IT(&hi2c1, i2c_transfer_out_node.dev_address << 1, i2c_transfer_out_node.command,
+                        1, i2c_transfer_out_node.data, i2c_transfer_out_node.num_data);
+            }
+            else // It's a write
+            {
+                hi2c1.Instance->CR2 |= I2C_CR2_PECBYTE;
+                HAL_I2C_Mem_Write_IT(&hi2c1, i2c_transfer_out_node.dev_address << 1, i2c_transfer_out_node.command,
+                        1, i2c_transfer_out_node.data, i2c_transfer_out_node.num_data + 1);
+            }
+      }
 
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
   }
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
   /* USER CODE END 3 */
 }
 
@@ -310,29 +266,29 @@ static void MX_CAN_Init(void)
   }
   /* USER CODE BEGIN CAN_Init 2 */
 
-  //  Enable FIFO0 Message Pending Interrupt and register corresponding callback
-  //  Used for activating/deactivating solenoids
-  HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING);
-  HAL_CAN_RegisterCallback(&hcan, HAL_CAN_RX_FIFO0_MSG_PENDING_CB_ID, CAN_FIFO0_RXMessagePendingCallback);
-  CAN_ConfigureFilterForCanRecvOperation(
-		  SOLENOID_OPERATION_CAN_ID,
-		  SOLENOID_OPERATION_CAN_FIFO_NUMBER,
-		  SOLENOID_OPERATION_CAN_FILTER_BANK_NUMBER);
+    //  Enable FIFO0 Message Pending Interrupt and register corresponding callback
+    //  Used for activating/deactivating solenoids
+    HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING);
+    HAL_CAN_RegisterCallback(&hcan, HAL_CAN_RX_FIFO0_MSG_PENDING_CB_ID, CAN_FIFO0_RXMessagePendingCallback);
+    CAN_ConfigureFilterForCanRecvOperation(
+        SOLENOID_OPERATION_CAN_ID,
+        SOLENOID_OPERATION_CAN_FIFO_NUMBER,
+        SOLENOID_OPERATION_CAN_FILTER_BANK_NUMBER);
 
-  //  Enable FIFO1 Message Pending Interrupt and register corresponding callback
-  //  Used for receiving Power Brick Communication Queries
-  HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO1_MSG_PENDING);
-  HAL_CAN_RegisterCallback(&hcan, HAL_CAN_RX_FIFO1_MSG_PENDING_CB_ID, CAN_FIFO1_RXMessagePendingCallback);
-  CAN_ConfigureFilterForCanRecvOperation(
-		  POWER_BRICK_OPERATION_CAN_ID,
-		  POWER_BRICK_OPERATION_CAN_FIFO_NUMBER,
-		  POWER_BRICK_OPERATION_CAN_FILTER_BANK_NUMBER);
+    //  Enable FIFO1 Message Pending Interrupt and register corresponding callback
+    //  Used for receiving Power Brick Communication Queries
+    HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO1_MSG_PENDING);
+    HAL_CAN_RegisterCallback(&hcan, HAL_CAN_RX_FIFO1_MSG_PENDING_CB_ID, CAN_FIFO1_RXMessagePendingCallback);
+    CAN_ConfigureFilterForCanRecvOperation(
+        POWER_BRICK_OPERATION_CAN_ID,
+        POWER_BRICK_OPERATION_CAN_FIFO_NUMBER,
+        POWER_BRICK_OPERATION_CAN_FILTER_BANK_NUMBER);
 
-  //  Enable TX Request Complete Interrupt
-  HAL_CAN_ActivateNotification(&hcan, CAN_IT_TX_MAILBOX_EMPTY);
-  HAL_CAN_RegisterCallback(&hcan, HAL_CAN_TX_MAILBOX0_COMPLETE_CB_ID, CAN_TxRequestCompleteCallback);
-  HAL_CAN_RegisterCallback(&hcan, HAL_CAN_TX_MAILBOX1_COMPLETE_CB_ID, CAN_TxRequestCompleteCallback);
-  HAL_CAN_RegisterCallback(&hcan, HAL_CAN_TX_MAILBOX2_COMPLETE_CB_ID, CAN_TxRequestCompleteCallback);
+    //  Enable TX Request Complete Interrupt
+    HAL_CAN_ActivateNotification(&hcan, CAN_IT_TX_MAILBOX_EMPTY);
+    HAL_CAN_RegisterCallback(&hcan, HAL_CAN_TX_MAILBOX0_COMPLETE_CB_ID, CAN_TxRequestCompleteCallback);
+    HAL_CAN_RegisterCallback(&hcan, HAL_CAN_TX_MAILBOX1_COMPLETE_CB_ID, CAN_TxRequestCompleteCallback);
+    HAL_CAN_RegisterCallback(&hcan, HAL_CAN_TX_MAILBOX2_COMPLETE_CB_ID, CAN_TxRequestCompleteCallback);
 
   /* USER CODE END CAN_Init 2 */
 
@@ -439,141 +395,146 @@ static void MX_GPIO_Init(void)
 
 void CAN_ConfigureFilterForCanRecvOperation(uint32_t canId, uint32_t fifoNumber, uint32_t filterBankNumber)
 {
-	CAN_FilterTypeDef thrusterOperationFilter;
-	CAN_FilterBank canFilterBank;
-	CAN_FilterIDMaskConfig canFilterId;
-	CAN_FilterIDMaskConfig canFilterMask;
+    CAN_FilterTypeDef thrusterOperationFilter;
+    CAN_FilterBank canFilterBank;
+    CAN_FilterIDMaskConfig canFilterId;
+    CAN_FilterIDMaskConfig canFilterMask;
 
-	//  Configure Filter Bank Parameters except ID and Mask
-	thrusterOperationFilter.FilterFIFOAssignment = fifoNumber;
-	thrusterOperationFilter.FilterBank = filterBankNumber;
-	thrusterOperationFilter.FilterMode = CAN_FILTERMODE_IDMASK;
-	thrusterOperationFilter.FilterScale = CAN_FILTERSCALE_32BIT;
-	thrusterOperationFilter.FilterActivation = CAN_FILTER_ENABLE;
+    //  Configure Filter Bank Parameters except ID and Mask
+    thrusterOperationFilter.FilterFIFOAssignment = fifoNumber;
+    thrusterOperationFilter.FilterBank = filterBankNumber;
+    thrusterOperationFilter.FilterMode = CAN_FILTERMODE_IDMASK;
+    thrusterOperationFilter.FilterScale = CAN_FILTERSCALE_32BIT;
+    thrusterOperationFilter.FilterActivation = CAN_FILTER_ENABLE;
 
-	//  Configure only ID and Mask Filter Bank Parameters
-	canFilterBank.filterMode = CANIDFilterMode_32BitMask;
+    //  Configure only ID and Mask Filter Bank Parameters
+    canFilterBank.filterMode = CANIDFilterMode_32BitMask;
 
-	canFilterId.stdId = canId;
-	canFilterId.extId = 0;
-	canFilterId.ide = CAN_IDE_CLEAR;
-	canFilterId.rtr = CAN_RTR_CLEAR;
+    canFilterId.stdId = canId;
+    canFilterId.extId = 0;
+    canFilterId.ide = CAN_IDE_CLEAR;
+    canFilterId.rtr = CAN_RTR_CLEAR;
 
-	canFilterMask.stdId = 0x7FF;
-	canFilterMask.extId = 0;
-	canFilterMask.ide = CAN_IDE_CLEAR;
-	canFilterMask.rtr = CAN_RTR_CLEAR;
+    canFilterMask.stdId = 0x7FF;
+    canFilterMask.extId = 0;
+    canFilterMask.ide = CAN_IDE_CLEAR;
+    canFilterMask.rtr = CAN_RTR_CLEAR;
 
-	canFilterBank.id1 = &canFilterId;
-	canFilterBank.mask1 = &canFilterMask;
+    canFilterBank.id1 = &canFilterId;
+    canFilterBank.mask1 = &canFilterMask;
 
-	CAN_ConfigureFilterBank(&thrusterOperationFilter, &canFilterBank);
-	HAL_CAN_ConfigFilter(&hcan, &thrusterOperationFilter);
+    CAN_ConfigureFilterBank(&thrusterOperationFilter, &canFilterBank);
+    HAL_CAN_ConfigFilter(&hcan, &thrusterOperationFilter);
 }
 
 //  Handles ONLY the reception of solenoid Operation Packets
 void CAN_FIFO0_RXMessagePendingCallback(CAN_HandleTypeDef *_hcan)
 {
-	uint8_t data[4];
+	uint32_t data32;
+    uint8_t *data = (uint8_t *)&data32;
 
-	data[0] = (uint8_t)((CAN_RDL0R_DATA0 & _hcan->Instance->sFIFOMailBox[0].RDLR) >> CAN_RDL0R_DATA0_Pos);
-	data[1] = (uint8_t)((CAN_RDL0R_DATA1 & _hcan->Instance->sFIFOMailBox[0].RDLR) >> CAN_RDL0R_DATA1_Pos);
-	data[2] = (uint8_t)((CAN_RDL0R_DATA2 & _hcan->Instance->sFIFOMailBox[0].RDLR) >> CAN_RDL0R_DATA2_Pos);
-	data[3] = (uint8_t)((CAN_RDL0R_DATA3 & _hcan->Instance->sFIFOMailBox[0].RDLR) >> CAN_RDL0R_DATA3_Pos);
+    data32 = CAN_RDL0R_DATA0 & _hcan->Instance->sFIFOMailBox[0].RDLR;
+    /*
+     * NOTE: NEED TO CHECK IF TRICK TO PERFORM FEWER REGISTER READS SWITCHES DATA ORDER DUE TO ENDIANNESS.
+    data[0] = (uint8_t)((CAN_RDL0R_DATA0 & _hcan->Instance->sFIFOMailBox[0].RDLR) >> CAN_RDL0R_DATA0_Pos);
+    data[1] = (uint8_t)((CAN_RDL0R_DATA1 & _hcan->Instance->sFIFOMailBox[0].RDLR) >> CAN_RDL0R_DATA1_Pos);
+    data[2] = (uint8_t)((CAN_RDL0R_DATA2 & _hcan->Instance->sFIFOMailBox[0].RDLR) >> CAN_RDL0R_DATA2_Pos);
+    data[3] = (uint8_t)((CAN_RDL0R_DATA3 & _hcan->Instance->sFIFOMailBox[0].RDLR) >> CAN_RDL0R_DATA3_Pos);
+    */
 
-	/*
-	 * Solenoid Control Code
-	 */
+    /*
+     * Solenoid Control Code
+     */
 
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
-	// HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_15);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
+    // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_15);
 
-	//  Release Output Mailbox
-	SET_BIT(_hcan->Instance->RF0R, CAN_RF0R_RFOM0);
+    //  Release Output Mailbox
+    SET_BIT(_hcan->Instance->RF0R, CAN_RF0R_RFOM0);
 }
 
 //  Handles ONLY to reception of power brick communication packets
 void CAN_FIFO1_RXMessagePendingCallback(CAN_HandleTypeDef *_hcan)
 {
-	uint8_t data[4];
-	uint32_t numBytesReceived;
-	I2CTxData i2cTxData;
+    uint8_t data[4];
+    uint32_t numBytesReceived;
+    I2CTxData i2cTxData;
 
-	numBytesReceived = (CAN_RDT1R_DLC & _hcan->Instance->sFIFOMailBox[1].RDTR) >> CAN_RDT1R_DLC_Pos;
+    numBytesReceived = (CAN_RDT1R_DLC & _hcan->Instance->sFIFOMailBox[1].RDTR) >> CAN_RDT1R_DLC_Pos;
 
-	data[0] = (uint8_t)((CAN_RDL1R_DATA0 & _hcan->Instance->sFIFOMailBox[1].RDLR) >> CAN_RDL1R_DATA0_Pos);
-	data[1] = (uint8_t)((CAN_RDL1R_DATA1 & _hcan->Instance->sFIFOMailBox[1].RDLR) >> CAN_RDL1R_DATA1_Pos);
-	data[2] = (uint8_t)((CAN_RDL1R_DATA2 & _hcan->Instance->sFIFOMailBox[1].RDLR) >> CAN_RDL1R_DATA2_Pos);
-	data[3] = (uint8_t)((CAN_RDL1R_DATA3 & _hcan->Instance->sFIFOMailBox[1].RDLR) >> CAN_RDL1R_DATA3_Pos);
+    data[0] = (uint8_t)((CAN_RDL1R_DATA0 & _hcan->Instance->sFIFOMailBox[1].RDLR) >> CAN_RDL1R_DATA0_Pos);
+    data[1] = (uint8_t)((CAN_RDL1R_DATA1 & _hcan->Instance->sFIFOMailBox[1].RDLR) >> CAN_RDL1R_DATA1_Pos);
+    data[2] = (uint8_t)((CAN_RDL1R_DATA2 & _hcan->Instance->sFIFOMailBox[1].RDLR) >> CAN_RDL1R_DATA2_Pos);
+    data[3] = (uint8_t)((CAN_RDL1R_DATA3 & _hcan->Instance->sFIFOMailBox[1].RDLR) >> CAN_RDL1R_DATA3_Pos);
 
-	//  Release Output Mailbox
-	SET_BIT(_hcan->Instance->RF1R, CAN_RF1R_RFOM1);
+    //  Release Output Mailbox
+    SET_BIT(_hcan->Instance->RF1R, CAN_RF1R_RFOM1);
 
-	i2cTxData.data[0] = data[2];
-	i2cTxData.data[1] = data[3];
-	i2cTxData.command = (uint16_t)data[1];
-	i2cTxData.dev_address = ((data[0] & 0x01) != 0x00) ? I2C_BRICK_1_DEVICE_ADDRESS : I2C_BRICK_0_DEVICE_ADDRESS;
-	i2cTxData.read_write = (data[0] & 0x02) >> 1;
-	i2cTxData.num_data = numBytesReceived - 2;
-	AddToQueue(i2cTxQueueHandle, &i2cTxData);
+    i2cTxData.data[0] = data[2];
+    i2cTxData.data[1] = data[3];
+    i2cTxData.command = (uint16_t)data[1];
+    i2cTxData.dev_address = ((data[0] & 0x01) != 0x00) ? I2C_BRICK_1_DEVICE_ADDRESS : I2C_BRICK_0_DEVICE_ADDRESS;
+    i2cTxData.read_write = (data[0] & 0x02) >> 1;
+    i2cTxData.num_data = numBytesReceived - 2;
+    AddToQueue(i2cTxQueueHandle, &i2cTxData);
 
-	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
-	// HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_15);
+    //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
+    // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_15);
 }
 
 void SendCANMessage(CanTxData* canTxDataToSend)
 {
-	uint32_t txMailboxNumber;
+    uint32_t txMailboxNumber;
 
-	if (HAL_CAN_GetTxMailboxesFreeLevel(&hcan) > 0 && isQueueEmpty(canTxQueueHandle))
-	{
-		HAL_CAN_AddTxMessage(&hcan, &(canTxDataToSend->canTxHeader), canTxDataToSend->data, &txMailboxNumber);
-	}
-	else
-	{
-		if (AddToQueue(canTxQueueHandle, canTxDataToSend) != QUEUE_SUCCESS)
-		{
-			; //  Queue is Full
-		}
-	}
+    if (HAL_CAN_GetTxMailboxesFreeLevel(&hcan) > 0 && isQueueEmpty(canTxQueueHandle))
+    {
+        HAL_CAN_AddTxMessage(&hcan, &(canTxDataToSend->canTxHeader), canTxDataToSend->data, &txMailboxNumber);
+    }
+    else
+    {
+        if (AddToQueue(canTxQueueHandle, canTxDataToSend) != QUEUE_SUCCESS)
+        {
+            ; //  Queue is Full
+        }
+    }
 }
 
 void CAN_TxRequestCompleteCallback(CAN_HandleTypeDef *_hcan)
 {
-	uint32_t txMailboxNumber;
-	CanTxData canTxDataToSend;
+    uint32_t txMailboxNumber;
+    CanTxData canTxDataToSend;
 
-	if (!isQueueEmpty(canTxQueueHandle) && HAL_CAN_GetTxMailboxesFreeLevel(_hcan) > 0)
-	{
-		RemoveFromQueue(canTxQueueHandle, (void*)&canTxDataToSend);
-		HAL_CAN_AddTxMessage(_hcan, &(canTxDataToSend.canTxHeader), canTxDataToSend.data, &txMailboxNumber);
-	}
+    if (!isQueueEmpty(canTxQueueHandle) && HAL_CAN_GetTxMailboxesFreeLevel(_hcan) > 0)
+    {
+        RemoveFromQueue(canTxQueueHandle, (void*)&canTxDataToSend);
+        HAL_CAN_AddTxMessage(_hcan, &(canTxDataToSend.canTxHeader), canTxDataToSend.data, &txMailboxNumber);
+    }
 }
 
 void I2C_TransactionCompleteCallback(I2C_HandleTypeDef *hi2c)
 {
-	CanTxData canTxMessage;
+    CanTxData canTxMessage;
 
-	canTxMessage.canTxHeader.StdId = POWER_BRICK_REPLY_CAN_ID;
-	canTxMessage.canTxHeader.DLC = i2c_transfer_out_node.num_data + 2;
-	canTxMessage.canTxHeader.IDE = CAN_ID_STD;
-	canTxMessage.canTxHeader.RTR = CAN_RTR_DATA;
+    canTxMessage.canTxHeader.StdId = POWER_BRICK_REPLY_CAN_ID;
+    canTxMessage.canTxHeader.DLC = i2c_transfer_out_node.num_data + 2;
+    canTxMessage.canTxHeader.IDE = CAN_ID_STD;
+    canTxMessage.canTxHeader.RTR = CAN_RTR_DATA;
 
-	canTxMessage.data[0] = i2c_transfer_out_node.num_data << 2;
-	canTxMessage.data[0] |= i2c_transfer_out_node.read_write << 1;
-	canTxMessage.data[0] |= (i2c_transfer_out_node.dev_address == I2C_BRICK_0_DEVICE_ADDRESS) ? 0 : 1;
+    canTxMessage.data[0] = i2c_transfer_out_node.num_data << 2;
+    canTxMessage.data[0] |= i2c_transfer_out_node.read_write << 1;
+    canTxMessage.data[0] |= (i2c_transfer_out_node.dev_address == I2C_BRICK_0_DEVICE_ADDRESS) ? 0 : 1;
 
-  	canTxMessage.data[1] = i2c_transfer_out_node.command;
-  	for (int i = 0; i < i2c_transfer_out_node.num_data; i++)
-  	{
-  		canTxMessage.data[i + 2] = i2c_transfer_out_node.data[i];
-  	}
+    canTxMessage.data[1] = i2c_transfer_out_node.command;
+    for (int i = 0; i < i2c_transfer_out_node.num_data; i++)
+    {
+        canTxMessage.data[i + 2] = i2c_transfer_out_node.data[i];
+    }
 
-  	HAL_NVIC_DisableIRQ(CEC_CAN_IRQn);
-  	SendCANMessage(&canTxMessage);
-  	HAL_NVIC_EnableIRQ(CEC_CAN_IRQn);
+    HAL_NVIC_DisableIRQ(CEC_CAN_IRQn);
+    SendCANMessage(&canTxMessage);
+    HAL_NVIC_EnableIRQ(CEC_CAN_IRQn);
 
-  	i2cTransactionReadyToStart = 1;
+    i2cTransactionReadyToStart = 1;
 }
 
 /* USER CODE END 4 */
